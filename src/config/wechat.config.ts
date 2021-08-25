8db775e231852628904d7be53ab6bb4de49bc779
王月 <yuewang@upchina.com>
2021-08-25 15:28:04 +0800

    :sparkles:微信api

--- src/config/wechat.config.ts
+++ src/config/wechat.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('wechat', () => ({
-  appID: process.env.AppID,
-  appSecret: process.env.AppSecret,
+  appID: process.env.WECHAT_APPID,
+  appSecret: process.env.WECHAT_APPSECRET,
 }));
